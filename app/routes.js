2ef6df751dd3b33b07bda63a63dc255190722593
seanng <shonum@gmail.com>
2017-01-19 10:20:50 +0800

    Add chats to routes

--- app/routes.js
+++ app/routes.js
@@ -67,6 +67,26 @@ export default function createRoutes(store) {
         importModules.catch(errorLoading);
       },
     }, {
+      path: 'chats',
+      name: 'chats',
+      getComponent(nextState, cb) {
+        const importModules = Promise.all([
+          import('containers/Chats/reducer'),
+          import('containers/Chats/sagas'),
+          import('containers/Chats'),
+        ]);
+
+        const renderRoute = loadModule(cb);
+
+        importModules.then(([reducer, sagas, component]) => {
+          injectReducer('chats', reducer.default);
+          injectSagas(sagas.default);
+          renderRoute(component);
+        });
+
+        importModules.catch(errorLoading);
+      },
+    }, {
       path: '*',
       name: 'notfound',
       getComponent(nextState, cb) {
