d4ab46191f56862ba319e9b678636a53a229aad4
seanng <shonum@gmail.com>
2017-01-19 16:44:16 +0800

    Add styling and clickhandling logic to SelectPostType

--- app/components/SelectPostType/index.js
+++ app/components/SelectPostType/index.js
@@ -10,11 +10,29 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
-function SelectPostType() {
+import H2 from 'components/H2';
+
+function SelectPostType({selected, clickPostType}) {
+
+  const classType = (type) => selected === type ? 'form-option-selected' : 'form-option-unselected';
+
   return (
-    <div>
-      'selectposttype'
-      <FormattedMessage {...messages.header} />
+    <div className="row">
+      <FormattedMessage {...messages.prompt} />
+      <div className="row">
+        <div onClick={clickPostType.bind(this, 'temporary')} className={`col-sm-6 ${classType('temporary')}`}>
+          <H2>
+            <FormattedMessage {...messages.tempOptionTitle} />
+          </H2>
+          <FormattedMessage {...messages.tempOptionDesc} />
+        </div>
+        <div onClick={clickPostType.bind(this, 'permanent')} className={`col-sm-6 ${classType('permanent')}`}>
+          <H2>
+            <FormattedMessage {...messages.permOptionTitle} />
+          </H2>
+          <FormattedMessage {...messages.permOptionDesc} />
+        </div>
+      </div>
     </div>
   );
 }
--- app/components/SelectPostType/messages.js
+++ app/components/SelectPostType/messages.js
@@ -10,4 +10,24 @@ export default defineMessages({
     id: 'app.components.SelectPostType.header',
     defaultMessage: 'This is the SelectPostType component !',
   },
+  prompt: {
+    id: 'app.components.SelectPostType.prompt',
+    defaultMessage: 'What type of job are you posting?',
+  },
+  tempOptionTitle: {
+    id: 'app.components.SelectPostType.tempOptionTitle',
+    defaultMessage: 'Temporary',
+  },
+  tempOptionDesc: {
+    id: 'app.components.SelectPostType.tempOptionDesc',
+    defaultMessage: 'This type of post is for hiring one off workers and temporary staff.\nThere would be a specific start and end time.',
+  },
+  permOptionTitle: {
+    id: 'app.components.SelectPostType.permOptionTitle',
+    defaultMessage: 'Permanent',
+  },
+  permOptionDesc: {
+    id: 'app.components.SelectPostType.permOptionDesc',
+    defaultMessage: 'This type of post is for hiring freelance, part time or full time staff.\nThere is no specific start and end time.',
+  },
 });
